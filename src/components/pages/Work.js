2c8b48264ca8d672656ca5dfa752a3f06ceca106
provole <volen.angelov@gmail.com>
2021-02-18 20:02:27 +0000

    Portfolio grid items

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -30,28 +30,69 @@ export default function Work() {
         <div className="my-work">
             <div className="gridbox">
                 <div className="gridbox__item">
-                    <img src="https://picsum.photos/400/300" />
+                    <figure class="effect-layla">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/6.jpg" alt="img06"/>
+						<figcaption>
+							<h2>Epitome <br />Digital</h2>
+							<a href="https://epitome.digital/">View more</a>
+						</figcaption>			
+					</figure>
                 </div>
                 <div className="gridbox__item">
-                    <img src="https://picsum.photos/400/300" />
+                    <figure class="effect-layla">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/26.jpg" alt="img06"/>
+						<figcaption>
+							<h2>Pragmatic <br />Digital</h2>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
                 </div>
                 <div className="gridbox__item">
-                    <img src="https://picsum.photos/400/300" />
+                    <figure class="effect-layla">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/24.jpg" alt="img06"/>
+						<figcaption>
+							<h2>Michelin </h2>
+							<a href="https://marketing.michelin.co.uk/return_to_work/give-us-feedback.php">View more</a>
+						</figcaption>			
+					</figure>
                 </div>
                 <div className="gridbox__item">
-                    <img src="https://picsum.photos/400/300" />
+                    <figure class="effect-layla">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/23.jpg" alt="img06"/>
+						<figcaption>
+							<h2>SoAmpli</h2>
+							<a href="https://www.soampli.com/">View more</a>
+						</figcaption>			
+					</figure>
                 </div>
                 <div className="gridbox__item">
-                    <img src="https://picsum.photos/400/300" />
+                    <figure class="effect-layla">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/22.jpg" alt="img06"/>
+						<figcaption>
+							<h2>Force24 <br /> Tools</h2>
+							<a href="http://frontendstaging.force24.co.uk/tools/">View more</a>
+						</figcaption>			
+					</figure>
                 </div>
                 <div className="gridbox__item">
-                    <img src="https://picsum.photos/400/300" />
+                    <figure class="effect-layla">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/21.jpg" alt="img06"/>
+						<figcaption>
+							<h2>TruNarrative</h2>
+							<a href="https://frontendstaging.force24.co.uk/trunarrative/">View more</a>
+						</figcaption>			
+					</figure>
                 </div>
                 <div className="gridbox__item">
-                    <img src="https://picsum.photos/400/300" />
+                    <figure class="effect-layla">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/20.jpg" alt="img06"/>
+						<figcaption>
+							<h2>PRAGMATIC <br /><span>DIGITAL</span></h2>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
                 </div>
             </div>
-            
         </div>
     </div>
   );
